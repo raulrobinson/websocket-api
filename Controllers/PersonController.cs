309c43b2615ba309d2dba2776ba5c76c04c1a7f8
rasysbox <rasysbox@hotmail.com>
2024-04-03 11:38:55 -0500

    fc

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebSocket.Domain.Dtos;
+using WebSocket.Domain.Interfaces;
+
+namespace WebSocket.Api.Controller;
+
+
+[ApiController]
+[Route("/api/v1/person")]
+public class PersonController
+{
+    private readonly IPersonServices _personServices;
+
+    public PersonController(IPersonServices personServices)
+    {
+        _personServices = personServices;
+    }
+
+    [HttpPost("/register")]
+    public async Task<IResult> create(RegisterPersonDto person)
+    {
+        return Results.Ok(await _personServices.RegisterPerson(person));
+    }
+    
+    [HttpPost("/login")]
+    public async Task<IResult> login(PersonLoginDto person)
+    {
+        return Results.Ok(await _personServices.LoginPerson(person));
+    }
+}
